814b35894110cea25b43930281f217efae8d6ccb
Myuuiii <mute@mutedevs.nl>
2021-06-22 13:17:11 +0200

    WIP

--- E14/reactreduxdemo/src/App.js
+++ E14/reactreduxdemo/src/App.js
@@ -3,6 +3,8 @@ import { Provider } from 'react-redux';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 import './App.css';
 import store from './redux/store';
 
@@ -10,8 +12,11 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer />
         <HooksCakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
